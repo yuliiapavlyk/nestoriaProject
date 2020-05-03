ddbf836187171b50af1dbfabb63bace22326ffbe
yuliiapavlyk <pavlyk.yuliia@gmail.com>
2020-05-03 23:49:36 +0300

    initial commit for property search page and property listing page

---
+++ nestoria-project/src/app/modules/property-listing-page/property-listing.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-listing',
+  templateUrl: './property-listing.component.html',
+  styleUrls: ['./property-listing.component.css']
+})
+export class PropertyListingComponent implements OnInit {
+
+  public fakeProperty = {
+    price:'3200$',
+    location:'Lviv, Ukraine',
+    link:'https://res.cloudinary.com/homelike/image/upload/w_1900,c_fit,f_jpg/homelike-05/uploads/bd3a8aab9d1159a0e08435506c6a6ee2ae22e51e12d0e4c56cf80b0d2a728d80.jpeg',
+    bedCount: 2,
+    bathroomsCount: 3,
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
---
+++ nestoria-project/src/app/modules/property-listing-page/property-listing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PropertyListingComponent } from './property-listing.component';
+import { SharedComponentsModule } from '../../shared/components/module';
+
+
+
+@NgModule({
+  declarations: [PropertyListingComponent],
+  imports: [
+    CommonModule,
+    SharedComponentsModule
+  ]
+})
+export class PropertyListingModule { }
